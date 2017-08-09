03dcdc307353b25a0e2733a5a1704bc3859a940f
nag <nagabhushanamn@gmail.com>
2017-08-09 11:16:18 +0530

    access specifiers

---
+++ java-abs-encap/src/com/App1.java
@@ -0,0 +1,26 @@
+package com;
+
+import p2.E;
+
+public class App1 {
+
+	public static void main(String[] args) {
+
+		// A a1 = new A();
+		// a1.aObjMethod();
+
+		// B b1 = new B();
+		// b1.bObjMethod();
+
+		// C c1 = new C();
+		// c1.cObjMethod();
+
+		// D d1 = new D();
+		// d1.dObjMethod();
+
+		E e = new E();
+		e.eObjMethod();
+
+	}
+
+}
---
+++ java-abs-encap/src/p1/A.java
@@ -0,0 +1,28 @@
+package p1;
+
+public class A {
+
+	// a. variable(s)
+
+	public static int pubSta = 0;
+
+	private int pri = 1;
+	int de = 2;
+	protected int pro = 3;
+	public int pub = 4;
+
+	// b. constructor(s)
+
+	// c. method(s)
+
+	public void aObjMethod() {
+		System.out.println(pubSta);
+		System.out.println(pri);
+		System.out.println(de);
+		System.out.println(pro);
+		System.out.println(pub);
+	}
+
+	// d. inner class(s)
+
+}
